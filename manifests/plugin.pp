f3d82ba810476dab361b4265f8501ad9b57be2af
Fx <fpizzurro@nb-pizzurro.(none)>
2014-06-19 16:02:33 +0200

    aggiunto provider a exec

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -11,16 +11,19 @@ define vagrant::plugin (
     'ubuntu': {
       $install_command="vagrant plugin install $plugin_name"
       $unless="vagrant plugin list | grep '$plugin_name'"
+      $provider=undef
     }
     'windows': {
       $install_command="vagrant plugin install $plugin_name"
       $unless="[bool](vagrant plugin list | Select-String '$plugin_name')"
+      $provider=powershell
     }
   }
 
   exec {"install vagrant plugin $plugin_name":
-    command => $install_command,
-    unless  => $unless
+    command   => $install_command,
+    unless    => $unless,
+    provider  => $provider
   }
 
 }
