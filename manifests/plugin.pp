5955d2cd3d232006962ebc78b98c48c12421e15e
Fx <fpizzurro@nb-pizzurro.(none)>
2014-06-19 16:39:02 +0200

    aggiunta home

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,5 +1,6 @@
 define vagrant::plugin (
   $plugin = '',
+  $home   = '',
 ) {
   
   $plugin_name = $plugin? {
@@ -12,18 +13,27 @@ define vagrant::plugin (
       $install_command="vagrant plugin install $plugin_name"
       $unless="vagrant plugin list | grep '$plugin_name'"
       $provider=undef
+
+      $env_librarian = $home? {
+        ''      => undef,
+        default => ["HOME=$home"]
+      }
     }
     'windows': {
       $install_command="vagrant plugin install $plugin_name"
       $unless="[bool](vagrant plugin list | Select-String '$plugin_name')"
       $provider=powershell
+
+      $env_librarian = undef
     }
   }
 
   exec {"install vagrant plugin $plugin_name":
-    command   => $install_command,
-    unless    => $unless,
-    provider  => $provider
+    command     => $install_command,
+    unless      => $unless,
+    provider    => $provider,
+    path        => $::path,
+    environment => $env_librarian
   }
 
 }
